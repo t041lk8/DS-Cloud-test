2591136f578cd922f21e4f59b79a1735e7e793d2
Zhbanov Ilya <t041lk8@yandex.ru>
2024-01-08 13:07:52 +0300

    Обновил файл для kserve

--- kserve/app.py
+++ kserve/app.py
@@ -6,28 +6,20 @@ import torch
 from transformers import BertForTokenClassification, BertTokenizerFast
 
 unique_tags = {'B-LOC', 'B-ORG', 'B-PER', 'I-LOC', 'I-ORG', 'I-PER', 'O'}
-labels_to_ids = {k: v for v, k in enumerate(unique_tags)}
-ids_to_labels = {v: k for v, k in enumerate(unique_tags)}
-
-def beauty_visual(preds, offset_mapping):
-    prediction = ''
-    for token_pred, mapping in zip(preds, offset_mapping.squeeze().tolist()):
-        if token_pred[0][0] =='[':
-            continue
-        pred = ''
-        if token_pred[1][0] == 'B':
-            prediction += ' '
-            pred = token_pred[1][2:]
-        elif token_pred[1][0] == 'I' and token_pred[0][:2] != '##':
-            pred = '-' * len(token_pred[0]) + '-'
-        elif token_pred[0][:2] == '##' or token_pred[1][0] == 'I':
-            pred = '-' * len(token_pred[0][3:])
-        else:
-            prediction += ' '
-            pred = token_pred[1]
-        pred += (mapping[1]-mapping[0] - len(pred)) * '-'
-        prediction += pred
-    return prediction[1:]
+ids_to_labels = {0: 'I-PER',
+  1: 'I-LOC',
+  2: 'B-LOC',
+  3: 'B-PER',
+  4: 'B-ORG',
+  5: 'O',
+  6: 'I-ORG'}
+labels_to_ids = {'I-PER': 0,
+  'I-LOC': 1,
+  'B-LOC': 2,
+  'B-PER': 3,
+  'B-ORG': 4,
+  'O': 5,
+  'I-ORG': 6}
 
 class MyModel(kserve.Model):
     def __init__(self, name: str):
@@ -44,8 +36,6 @@ class MyModel(kserve.Model):
         cur_start = 0
         cur_end = 0
         for (token, pred), (start, stop) in zip(tokens_preds, offset_mapping.squeeze().tolist()):
-            if token.startswith("["):
-                continue
             if token.startswith("##"):
                 ent_text += token[2:]
                 cur_end = stop
@@ -57,7 +47,7 @@ class MyModel(kserve.Model):
                 entities += [{'text': ent_text, 'type': ent_type, 'start': cur_start, 'end':cur_end}]
                 ent_text, ent_type, cur_start, cur_end = token, pred, start, stop
             if pred.startswith("I"):
-                ent_text += token
+                ent_text += ' ' + token
                 cur_end = stop
         return entities[1:]                       
 
@@ -76,8 +66,10 @@ class MyModel(kserve.Model):
         logits = outputs[0]
         logits = logits.view(-1, len(unique_tags))
         preds = torch.argmax(logits, axis=1)
+        preds = preds[1:mask[mask==1].shape[0]]
 
         tokens = self.tokenizer.convert_ids_to_tokens(ids.view(-1))
+        tokens = tokens[1:mask[mask==1].shape[0]]
         preds = [ids_to_labels[i] for i in preds.cpu().numpy()]
         tokens_preds = list(zip(tokens, preds))
         entities = self.get_prediction(tokens_preds, inputs["offset_mapping"])
